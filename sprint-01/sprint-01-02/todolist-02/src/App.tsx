c96901ddc5b7201f0b40e257cae1a46b01752b3a
IvanKishDev <aphrodisiak7@gmail.com>
2026-09-05 21:36:48 +0300

    first commit

---
+++ sprint-01/sprint-01-02/todolist-02/src/App.tsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import './App.css'
+import {TodolistItem} from './TodolistItem'
+
+export type FilterValues = 'all' | 'completed' | 'active'
+
+export type Task = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+export const App = () => {
+  const changeFilter = (filter: FilterValues) => {
+    setFilter(filter)
+  }
+
+  const [filter, setFilter] = useState<FilterValues>('all')
+
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'ReactJS', isDone: false },
+    { id: 4, title: 'Redux', isDone: false },
+    { id: 5, title: 'Typescript', isDone: false },
+    { id: 6, title: 'RTK query', isDone: false },
+  ])
+
+  let filteredTasks = tasks
+  if (filter === 'active') {
+    filteredTasks = tasks.filter(task => !task.isDone)
+  }
+  if (filter === 'completed') {
+    filteredTasks = tasks.filter(task => task.isDone)
+  }
+
+
+  const deleteTask = (taskId: number) => {
+    const filteredTasks = tasks.filter(task => {
+      return task.id !== taskId
+    })
+    setTasks(filteredTasks)
+  }
+
+  return (
+      <div className="app">
+        <TodolistItem title="What to learn"
+                      tasks={filteredTasks}
+                      deleteTask={deleteTask}
+                      changeFilter={changeFilter}/>
+      </div>
+  )
+}
